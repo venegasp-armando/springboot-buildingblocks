cfe454db61ae8361b6ec7cbadecad8fdf57ba6d4
Venegas Perez <avenegasperez@deloitte.com>
2024-06-20 17:09:52 -0600

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld")
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Armando", "Venegas", "Aguascalientes");
+	}
+}
